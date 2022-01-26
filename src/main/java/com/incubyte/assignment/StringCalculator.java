f22bbeb2fa53129f0ef5a7e6b1b69023b3303df1
gauthamsambath <gauthamsambat@gmail.com>
2022-01-27 03:06:50 +0530

    string calculator TDD : single number and multiple number addition test

--- src/main/java/com/incubyte/assignment/StringCalculator.java
+++ src/main/java/com/incubyte/assignment/StringCalculator.java
@@ -1,6 +1,8 @@
 package com.incubyte.assignment;
 
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -17,7 +19,9 @@ public class StringCalculator {
     public int add(String numbers) {
         if (StringUtils.isEmpty(numbers)) {
             return 0;
+        } else {
+            List<Integer> listOfIntegersExtracted = Arrays.stream(numbers.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+            return listOfIntegersExtracted.stream().reduce(Integer::sum).orElseThrow();
         }
-        return 1;
     }
 }
--- src/test/java/com/incubyte/assignment/StringCalculatorTest.java
+++ src/test/java/com/incubyte/assignment/StringCalculatorTest.java
@@ -30,5 +30,26 @@ public class StringCalculatorTest {
         assertEquals(0,stringCalculator.add(""));
     }
 
+    /**
+     * Test add method for single number.
+     */
+    @Test
+    public void testAddMethodForSingleNumber() {
+        assertEquals(1,stringCalculator.add("1"));
+        assertEquals(2,stringCalculator.add("2"));
+        assertEquals(3,stringCalculator.add("3"));
+        assertEquals(4,stringCalculator.add("4"));
+    }
+
+    /**
+     * Test add method for multiple numbers.
+     */
+    @Test
+    public void testAddMethodForMultipleNumbers() {
+        assertEquals(10,stringCalculator.add("1,4,5"));
+        assertEquals(10,stringCalculator.add("11,5,-6"));
+        assertEquals(21,stringCalculator.add("1,2,3,4,5,6"));
+    }
+
 }
 
