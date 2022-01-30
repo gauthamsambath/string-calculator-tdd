b1ada5605323e665a060ccf18dd40e7acc318f8b
gauthamsambath <gauthamsambat@gmail.com>
2022-01-31 00:02:46 +0530

    string calculator TDD : custom delimiters addition test

--- src/main/java/com/incubyte/assignment/StringCalculator.java
+++ src/main/java/com/incubyte/assignment/StringCalculator.java
@@ -2,8 +2,8 @@ package com.incubyte.assignment;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
-
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -22,8 +22,25 @@ public class StringCalculator {
         if (StringUtils.isEmpty(numbers)) {
             return 0;
         } else {
-            List<Integer> listOfIntegersExtracted = Arrays.stream(numbers.split(",|\\n")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+            String delimiter = numbers.split(",|\\n")[0];
+            if (StringUtils.isNumeric(delimiter)){
+                delimiter = ",";
+            }
+            else {
+                int delimiterLength = delimiter.split("").length;
+                delimiter = extractDelimiter(delimiter);
+                numbers = numbers.substring(delimiterLength);
+            }
+            String splitter = "["+delimiter+"]"+"|\\n";
+            List<Integer> listOfIntegersExtracted = Arrays.stream(numbers.split(splitter)).filter(a->!a.equals("")).map(Integer::parseInt).collect(Collectors.toList());
             return listOfIntegersExtracted.stream().reduce(Integer::sum).orElseThrow();
         }
     }
+
+    private String extractDelimiter(String delimiter) {
+        List<String> listOfStringsExtracted = Arrays.stream(delimiter.split("")).filter(a-> !Objects.equals(a, "/")).collect(Collectors.toList());
+        delimiter = String.join("",listOfStringsExtracted);
+        return delimiter;
+    }
+
 }
--- src/test/java/com/incubyte/assignment/StringCalculatorTest.java
+++ src/test/java/com/incubyte/assignment/StringCalculatorTest.java
@@ -62,5 +62,17 @@ public class StringCalculatorTest {
         assertEquals(1,stringCalculator.add("1\n"));
     }
 
+    /**
+     * Test add method with custom delimiters.
+     */
+    @Test
+    public void testAddMethodWithCustomDelimiters(){
+        assertEquals(3,stringCalculator.add("//;\n1;2"));
+        assertEquals(3,stringCalculator.add("//$\n1$2"));
+        assertEquals(3,stringCalculator.add("//*\n1*2"));
+        assertEquals(3,stringCalculator.add("//$#*\n1$#*2"));
+        assertEquals(6,stringCalculator.add("//$#*\n1$#*2$#*3"));
+    }
+
 }
 
